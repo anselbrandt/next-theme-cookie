1869dee5eb530190673b84af815890c0cf6cec48
ansel brandt <ansel@anselbrandt.com>
2022-03-15 21:18:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This [Next.js](https://nextjs.org/) project uses cookies and [`getServerSideProps`](https://nextjs.org/docs/api-reference/data-fetching/get-server-side-props) to preserve themeing when linking between pages and loading routes directly.
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ pages/index.tsx
@@ -0,0 +1,126 @@
+import "@fontsource/libre-baskerville";
+import { Box, ThemeProvider } from "@primer/react";
+import Cookie from "js-cookie";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import ColorModeSwitcher from "../components/ColorModeSwitcher";
+import { NextPrimerLink } from "../components/NextPrimerLink";
+
+declare type ColorMode = "day" | "night";
+declare type ColorModeWithAuto = ColorMode | "auto";
+
+interface Props {
+  preferredColorMode: ColorModeWithAuto;
+  preferredDayScheme: string;
+  preferredNightScheme: string;
+}
+
+const Home: NextPage<Props> = ({
+  preferredColorMode,
+  preferredDayScheme,
+  preferredNightScheme,
+}) => {
+  const router = useRouter();
+  const [colorMode, setColorMode] = useState<ColorModeWithAuto>(
+    preferredColorMode || "day"
+  );
+  const [dayScheme, setDayScheme] = useState(preferredDayScheme || "light");
+  const [nightScheme, setNightScheme] = useState(
+    preferredNightScheme || "dark"
+  );
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      const preferredMode = Cookie.get("colorMode");
+      if (preferredMode) {
+        if (preferredMode === "night") {
+          const preferredScheme = Cookie.get("nightScheme") as string;
+          setTimeout(() => {
+            setColorMode("night");
+            setDayScheme(preferredScheme);
+            setNightScheme(preferredScheme);
+          }, 50);
+        }
+        if (preferredMode === "day") {
+          const preferredScheme = Cookie.get("dayScheme") as string;
+          setTimeout(() => {
+            setColorMode("day");
+            setDayScheme(preferredScheme);
+            setNightScheme(preferredScheme);
+          }, 50);
+        }
+      }
+    }
+  }, []);
+
+  return (
+    <ThemeProvider
+      colorMode={colorMode}
+      dayScheme={dayScheme}
+      nightScheme={nightScheme}
+    >
+      <Box
+        height="100vh"
+        width="100vw"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        bg="canvas.default"
+        color="fg.default"
+        fontFamily="Libre Baskerville"
+      >
+        <ColorModeSwitcher />
+        <Box>
+          <Box m={2}>Index</Box>
+          <Box
+            m={2}
+            color="accent.fg"
+            sx={{
+              "&:hover": {
+                cursor: "pointer",
+                textDecoration: "undefline",
+              },
+            }}
+          >
+            <a href="/page2">Page 2 via link</a>
+          </Box>
+          <Box m={2}>
+            <NextPrimerLink href="/page2">Page 2 via NextLink</NextPrimerLink>
+          </Box>
+          <Box
+            m={2}
+            onClick={() => router.push("/page2")}
+            color="accent.fg"
+            sx={{
+              "&:hover": {
+                cursor: "pointer",
+                textDecoration: "undefline",
+              },
+            }}
+          >
+            Page 2 via router
+          </Box>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+export async function getServerSideProps(context: any) {
+  const cookies = context.req.cookies;
+  const colorMode = cookies && cookies.colorMode ? cookies.colorMode : "day";
+  const dayScheme = cookies && cookies.dayScheme ? cookies.dayScheme : "light";
+  const nightScheme =
+    cookies && cookies.nightScheme ? cookies.nightScheme : "dark";
+  return {
+    props: {
+      preferredColorMode: colorMode,
+      preferredDayScheme: dayScheme,
+      preferredNightScheme: nightScheme,
+    },
+  };
+}
+
+export default Home;
